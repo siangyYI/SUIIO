bbbe1357786305fa997f7a6db458c9f35d25c544
siangYI <b0975349939@gmail.com>
2021-08-30 01:47:34 +0800

    0830 弄好個人資料卡片了

--- src/compnents/Navbar.js
+++ src/compnents/Navbar.js
@@ -2,142 +2,147 @@ import React from "react";
 import "./Navbar.css";
 import Usercard from "./Usercard"
 import { Navbar, Nav, NavDropdown, Button } from "react-bootstrap";
+import { Component } from "react";
 
 const aText = { textDecoration: "none", ":hover": { textDecoration: "none" } };
 
-function NavBar() {
-  const [Card, setCard] = React.useState(false);
-  return (
-    <div>
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isToggleOn: true,
+      display: 'none'
+    };
+    this.handClick = this.handClick.bind(this);
+  }
+  handClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn,
+      display: prevState.isToggleOn ? 'none' : 'block'
+    }));
+  }
+  render() {
+    return (
+      <div>
 
-      <Navbar
-        collapseOnSelect
-        expand="lg"
-        bg="dark"
-        variant="dark"
-        className="phone"
-      >
+        <Navbar
+          collapseOnSelect
+          expand="lg"
+          bg="dark"
+          variant="dark"
+          className="phone"
+        >
 
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" style={{ color: "white" }}>
-          <Nav className="mr-auto" style={aText}>
-            <NavDropdown title="組織管理" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#/Organization/event">
-                活動
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#/Organization/cadre">
-                幹部
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#/Organization/member">
-                成員
-              </NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title="會議管理" id="basic-nav-dropdown">
-              <NavDropdown.Item exact href="/Index">會議記錄</NavDropdown.Item>
-              <NavDropdown.Item href="/Index/Uploadmeet">上傳記錄</NavDropdown.Item>
-              <NavDropdown.Item href="/Index/AuditList">審核會議</NavDropdown.Item>
-              <NavDropdown.Item href="/Index/MyList">我的上傳</NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title="財務管理" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#/Financial/statements">
-                財務報表
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#/Financial/records">
-                收支紀錄
-              </NavDropdown.Item>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav" style={{ color: "white" }}>
+            <Nav className="mr-auto" style={aText}>
+              <NavDropdown title="組織管理" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#/Organization/event">
+                  活動
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#/Organization/cadre">
+                  幹部
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#/Organization/member">
+                  成員
+                </NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="會議管理" id="basic-nav-dropdown">
+                <NavDropdown.Item exact href="/Index">會議記錄</NavDropdown.Item>
+                <NavDropdown.Item href="/Index/Uploadmeet">上傳記錄</NavDropdown.Item>
+                <NavDropdown.Item href="/Index/AuditList">審核會議</NavDropdown.Item>
+                <NavDropdown.Item href="/Index/MyList">我的上傳</NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="財務管理" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#/Financial/statements">
+                  財務報表
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#/Financial/records">
+                  收支紀錄
+                </NavDropdown.Item>
 
-              <NavDropdown.Item href="#/Financial/management">
-                預算管理
-              </NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title="留言管理" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/ReportMessage">收支留言</NavDropdown.Item>
-              <NavDropdown.Item href="/ReportMessage/#">財報留言</NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title="個人資料" id="basic-nav-dropdown"></NavDropdown>
-          </Nav>
-          <div className="d-flex mt-2">
-            <Button variant="outline-success">組織頁面</Button>&nbsp;
-            <Button variant="outline-success">登出</Button>
-          </div>
-        </Navbar.Collapse>
-      </Navbar>
-      <div className="d-flex justify-content-center mt-4">
-      <img
+                <NavDropdown.Item href="#/Financial/management">
+                  預算管理
+                </NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="留言管理" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/ReportMessage">收支留言</NavDropdown.Item>
+                <NavDropdown.Item href="/ReportMessage/#">財報留言</NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="個人資料" id="basic-nav-dropdown"></NavDropdown>
+            </Nav>
+            <div className="d-flex mt-2">
+              <Button variant="outline-success">組織頁面</Button>&nbsp;
+              <Button variant="outline-success">登出</Button>
+            </div>
+          </Navbar.Collapse>
+        </Navbar>
+        <div className="d-flex justify-content-center mt-4 ">
+          <img
             src={require("../Image/bell.png").default}
             alt="cancel"
-            width= "25px" height= "25px"
+            width="25px" height="25px"
             className="mt-2"
-            onClick={() => setCard(true)}
           />
-        <img
+          <div className="d-flex flex-column">
+            <img
             src={require("../Image/user.png").default}
             alt="cancel"
-            width= "55px" height= "55px"
+            width="55px" height="55px"
             className=" mt-4"
-            onClick={() => setCard(true)}
+            onClick={this.handClick}
           />
-      </div>
-      
-      <aside>
-        <div className="my-4">
-          <img
-            src={require("../Image/home.png").default}
-            alt="cancel"
-            style={{ width: "30pt", height: "30pt" }}
-          />
-          <br />
-          <a href="#/Organization/event">組織管理</a>
+          <div style={{ display: this.state.display }}>
+            <Usercard />
+          </div>
+          </div>
+          
         </div>
-        <div className="my-4">
-          <img
-            src={require("../Image/note.png").default}
-            alt="cancel"
-            style={{ width: "30pt", height: "30pt" }}
-          />
-          <br />
-          <a href="#/">會議管理</a>
-        </div>
-        <div className="my-4">
-          <img
-            src={require("../Image/inbox.png").default}
-            alt="cancel"
-            style={{ width: "30pt", height: "30pt" }}
-          />
-          <br />
-          <a href="#/Financial/statements">財務管理</a>
-        </div>
-        <div className="my-4">
-          <img
-            src={require("../Image/bar-chart.png").default}
-            alt="cancel"
-            style={{ width: "30pt", height: "30pt" }}
-          />
-          <br />
-          <a href="#/ReportMessage">留言管理</a>
-        </div>
-      </aside> 
-      <div className="d-flex flex-column">
-        <button className="d-flex flex-row justify-content-center my-3 py-2 toolbutton">
-          <img
-            src={require("../Image/trun.png").default}
-            alt="cancel"
-            width="25px" height="25px"
-          />
-          <div className="d-flex align-items-center mx-auto">成員頁面</div>
-        </button>
 
-        <button className="d-flex flex-row justify-content-center py-2 toolbutton">
-          <img
-            src={require("../Image/logout.png").default}
-            alt="cancel"
-            width="25px" height="25px"
-          />
-          <div className="d-flex align-items-center mx-auto">登出</div>
-        </button>
+
+        <aside>
+          <div className="my-4">
+            <img
+              src={require("../Image/home.png").default}
+              alt="cancel"
+              style={{ width: "30pt", height: "30pt" }}
+            />
+            <br />
+            <a href="#/Organization/event">組織管理</a>
+          </div>
+          <div className="my-4">
+            <img
+              src={require("../Image/note.png").default}
+              alt="cancel"
+              style={{ width: "30pt", height: "30pt" }}
+            />
+            <br />
+            <a href="#/">會議管理</a>
+          </div>
+          <div className="my-4">
+            <img
+              src={require("../Image/inbox.png").default}
+              alt="cancel"
+              style={{ width: "30pt", height: "30pt" }}
+            />
+            <br />
+            <a href="#/Financial/statements">財務管理</a>
+          </div>
+          <div className="my-4">
+            <img
+              src={require("../Image/bar-chart.png").default}
+              alt="cancel"
+              style={{ width: "30pt", height: "30pt" }}
+            />
+            <br />
+            <a href="#/ReportMessage">留言管理</a>
+          </div>
+        </aside>
+        
+
       </div>
-      <Usercard show={Card} onHide={() => setCard(false)} />
-    </div>
-  );
+    );
+  }
+
 }
 export default NavBar;
--- src/compnents/Usercard.js
+++ src/compnents/Usercard.js
@@ -1,33 +1,74 @@
 import React, { Component } from "react";
-import { Button, Modal, Form } from "react-bootstrap";
+import './Usercard.css'
 class Usercard extends Component {
     render() {
         return (
             <>
-                <Modal {...this.props} size="md" centered>
-                    <Form onSubmit={this.add}>
-                        <Modal.Header closeButton>
-                            <Modal.Title>
-                                <strong>確定通過嗎？</strong>
-                            </Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body>
-                            <Form.Group>
-                                <Form.Label>
-                                    請多多確認
-                                </Form.Label>
-                            </Form.Group>
-                        </Modal.Body>
-                        <Modal.Footer>
-                            <Button variant="dark" type="submit" href="/Financial/management" style={{ color: "white" }}>
-                                確定
-                            </Button>
-                            <Button variant="light" onClick={this.props.onHide}>
-                                取消
-                            </Button>
-                        </Modal.Footer>
-                    </Form>
-                </Modal>
+
+                <div className="ucard ">
+                    <div className="d-flex flex-row px-5 py-3">
+                        <img
+                            src={require("../Image/1144760.png").default}
+                            alt="cancel"
+                            width="55px" height="55px"
+                            className="mr-5 ml-3"
+                            onClick={this.handClick}
+                        />
+                        <div className="d-flex flex-column">
+                            <div>詹翔壹</div>
+                            <div>暱稱：siangyi</div>
+                            <div>性別：男</div>
+                        </div>
+
+                    </div>
+
+                    <div className="ucardline mt-2">
+                        <div className="d-flex my-3 d-flex justify-content-center">
+                            <img
+                                src={require("../Image/pencil.png").default}
+                                alt="cancel"
+                                width="25px" height="25px"
+                                className="mr-2"
+                                onClick={this.handClick}
+                            />
+                            <div>更改密碼</div>
+
+                        </div>
+                        <div className="ucardline my-2"></div>
+                        <div className="d-flex my-3 d-flex justify-content-center">
+                            <img
+                                src={require("../Image/pencil.png").default}
+                                alt="cancel"
+                                width="25px" height="25px"
+                                className="mr-2"
+                                onClick={this.handClick}
+                            />
+                            <div>更改個人資料</div>
+
+                        </div>
+                        <div className="d-flex justify-content-center ucardline">
+                            <button className="d-flex flex-row justify-content-center my-3 py-2 mx-3 ucardbutton">
+                                <img
+                                    src={require("../Image/trun.png").default}
+                                    alt="cancel"
+                                    width="25px" height="25px"
+                                />
+                                <div className="d-flex align-items-center ml-1">成員頁面</div>
+                            </button>
+
+                            <button className="d-flex flex-row justify-content-center my-3 py-2 mx-3 ucardbutton">
+                                <img
+                                    src={require("../Image/logout.png").default}
+                                    alt="cancel"
+                                    width="25px" height="25px"
+                                />
+                                <div className="d-flex align-items-center ml-1">登出</div>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+
             </>
         );
     }
