e3c39482f3b06cdef1dea53510b408c7543b2226
ziyuhuangyyyyyyyy <ziyuhuang1007@gmail.com>
2021-10-19 20:39:34 +0800

    1019 圖表改版

---
+++ src/compnents/Front_Side/Frontchart/select_example.js
@@ -0,0 +1,21 @@
+{/* <select
+                className="mt-1 ml-3 px-2"
+                style={{
+                  borderRadius: "10px",
+                  backgroundColor: "white",
+                }}
+              >
+                <option value="none">--請選擇月分--</option>
+                <option value="Jan">一月</option>
+                <option value="Feb">二月</option>
+                <option value="Mar">三月</option>
+                <option value="Apr">四月</option>
+                <option value="May">五月</option>
+                <option value="Jun">六月</option>
+                <option value="Jul">七月</option>
+                <option value="Aug">八月</option>
+                <option value="Sep">九月</option>
+                <option value="Oct">十月</option>
+                <option value="Nov">十一月</option>
+                <option value="Dec">十二月</option>
+              </select> */}
