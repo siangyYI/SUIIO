ec937087456d97c383c4216c1292cb0e499d0dde
siangYI <b0975349939@gmail.com>
2021-10-25 00:47:25 +0800

    1025 圖表完成

--- src/compnents/Notifycard.js
+++ src/compnents/Notifycard.js
@@ -1,16 +1,34 @@
 import React, { Component } from "react";
 import './Notifycard.css'
 import NotifyItem from './NotifyItem'
-import io from 'socket.io-client'
-const socket = io('http://localhost:4000');
+// import io from 'socket.io-client'
+// const socket = io('http://localhost:4000');
 
-socket.on('組織負責人', (socket_content) => {
-    console.log(socket_content)
-})
-socket.on('1110634039', (socket_content) => {
-    console.log(socket_content)
-})
+
+
+
 class Notifycard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            notufy: {},
+            sid: 1110634039
+        }
+        // this.fetchContent(this.state.sid)
+        // socket.on('組織負責人', (socket_content) => {
+
+        //     this.setState({ notufy: socket_content })
+        //     console.log(this.state.notufy.content)
+        // })
+        // socket.on('1110634039', (socket_content) => {
+        //     console.log(socket_content)
+        // })
+    }
+    fetchContent = async (sid) => {
+        await fetch(`http://localhost:4000/api/events/fetch/comment/${sid}`)
+            .then((res) => res.json())
+            .then((data) => this.setState({ notufy: data }));
+    };
 
     render() {
         
@@ -18,7 +36,6 @@ class Notifycard extends Component {
             <>
                 <div className="overflow-auto ncard ">
                     <div className="">
-                        <NotifyItem />
                         <NotifyItem />
                         <NotifyItem />
                         <NotifyItem />
