cf782ed43bea482b98f04bb2f28f991e7b6d3a01
siangYI <b0975349939@gmail.com>
2021-10-24 14:57:57 +0800

    1024

--- src/compnents/Front_Side/FrontCompare/CompareIndex.js
+++ src/compnents/Front_Side/FrontCompare/CompareIndex.js
@@ -5,122 +5,6 @@ import { Bar, Pie, Line } from "react-chartjs-2";
 import CompareFilter from "./CompareFilter";
 import { CompareDetail } from "./CompareDetail";
 import { CompareDetailTwo } from "./CompareDetailTwo";
-const expendData = {
-  type: "line",
-  labels: ["七月", "八月", "九月", "十月"],
-  datasets: [
-    {
-      label: "109大迎新收入",
-      data: [65, 59, 80, 61],
-      fill: false,
-      borderColor: "#16a085",
-      tension: 0.1,
-      pointStyle: "circle",
-      pointRadius: 5,
-      pointBorderColor: "#16a085",
-      backgroundColor: "#16a085",
-    },
-    {
-      label: "108大迎新收入",
-      data: [60, 51, 75, 61],
-      fill: false,
-      borderColor: "#27ae60",
-      tension: 0.1,
-      pointStyle: "circle",
-      pointRadius: 5,
-      pointBorderColor: "#27ae60",
-      backgroundColor: "#27ae60",
-    },
-  ],
-};
-const incomeData = {
-  type: "line",
-  labels: ["七月", "八月", "九月", "十月"],
-  datasets: [
-    {
-      label: "109大迎新支出",
-      data: [80, 70, 75, 85],
-      fill: false,
-      borderColor: "#c0392b",
-      tension: 0.1,
-      pointStyle: "circle",
-      pointRadius: 5,
-      pointBorderColor: "#c0392b",
-      backgroundColor: "#c0392b",
-    },
-    {
-      label: "108大迎新支出",
-      data: [60, 51, 75, 61],
-      fill: false,
-      borderColor: "#f39c12",
-      tension: 0.1,
-      pointStyle: "circle",
-      pointRadius: 5,
-      pointBorderColor: "#f39c12",
-      backgroundColor: "#f39c12",
-    },
-  ],
-};
-const PieData1 = {
-  labels: ["收益", "折損"], //顯示區間名稱
-  datasets: [
-    {
-      label: "108學年度", // tootip 出現的名稱
-      lineTension: 0, // 曲線的彎度，設0 表示直線
-      backgroundColor: ["#CCBE98", "#448599"],
-      borderColor: ["#CCBE98", "#448599"],
-      borderWidth: 1,
-      data: [37000, 20000], // 資料
-      fill: false, // 是否填滿色彩
-    },
-  ],
-};
-const PieData = {
-  labels: ["收益", "折損"], //顯示區間名稱
-  datasets: [
-    {
-      label: "109學年度", // tootip 出現的名稱
-      lineTension: 0, // 曲線的彎度，設0 表示直線
-      backgroundColor: ["#5197D5", "#222D43"],
-      borderColor: ["#5197D5", "#222D43"],
-      borderWidth: 1,
-
-      data: [20000, 38000], // 資料
-      fill: false, // 是否填滿色彩
-    },
-  ],
-};
-const HorizontalData1 = {
-  labels: [
-    "水",
-    "場勘入園費",
-    "生活組用品和食材用品",
-    "值星帶",
-    "場地費尾款",
-    "紅包",
-  ], //顯示區間名稱
-  datasets: [
-    {
-      label: "108學年度", // tootip 出現的名稱
-      lineTension: 0, // 曲線的彎度，設0 表示直線
-      backgroundColor: "#07889b",
-      borderColor: "#07889b",
-      borderWidth: 1,
-      data: [4100, 2300, 7138, 1240, 60430, 3300], // 資料
-      fill: false, // 是否填滿色彩
-    },
-    {
-      label: "109學年度", // tootip 出現的名稱
-      lineTension: 0, // 曲線的彎度，設0 表示直線
-      backgroundColor: "#e37222",
-      borderColor: "#e37222",
-      borderWidth: 1,
-      data: [3200, 1400, 6238, 240, 57430], // 資料
-      fill: false, // 是否填滿色彩
-    },
-  ],
-};
-const name = document.getElementById("activity1");
 const scrollToAnchor = (name) => {
   if (name) {
     // 找到錨點
@@ -194,7 +78,7 @@ export class CompareIndex extends Component {
           </div>
           <div id="activity3" style={{ paddingtop: "0.25%" }}>
             <div className="Comtitle  ">
-              <div className="">淨利/損圓餅圖</div>
+              <div className="">圓餅圖</div>
             </div>
             <div className="row my-2">
               <div
@@ -213,7 +97,20 @@ export class CompareIndex extends Component {
                   </div>
                 </div>
                 <Pie
-                  data={PieData}
+                  data={{
+                    labels: ["收益", "折損"], //顯示區間名稱
+                    datasets: [
+                      {
+                        label: "108學年度", // tootip 出現的名稱
+                        lineTension: 0, // 曲線的彎度，設0 表示直線
+                        backgroundColor: ["#1abc9c", "#f39c12"],
+                        borderColor: ["#1abc9c", "#f39c12"],
+                        borderWidth: 1,
+                        data: [37000, 20000], // 資料
+                        fill: false, // 是否填滿色彩
+                      },
+                    ],
+                  }}
                   options={{
                     responsive: true,
                     maintainAspectRatio: true,
@@ -266,7 +163,20 @@ export class CompareIndex extends Component {
                   </div>
                 </div>
                 <Pie
-                  data={PieData1}
+                  data={{
+                    labels: ["收益", "折損"], //顯示區間名稱
+                    datasets: [
+                      {
+                        label: "108學年度", // tootip 出現的名稱
+                        lineTension: 0, // 曲線的彎度，設0 表示直線
+                        backgroundColor: ["#1abc9c", "#f39c12"],
+                        borderColor: ["#1abc9c", "#f39c12"],
+                        borderWidth: 1,
+                        data: [37000, 20000], // 資料
+                        fill: false, // 是否填滿色彩
+                      },
+                    ],
+                  }}
                   options={{
                     responsive: true,
                     maintainAspectRatio: true,
@@ -375,7 +285,36 @@ export class CompareIndex extends Component {
                 <div className="ml-2 charttitle">九月比較長條圖(單位:元)</div>
               </div>
               <Bar
-                data={HorizontalData1}
+                data={{
+                  labels: [
+                    "水",
+                    "場勘入園費",
+                    "生活組用品和食材用品",
+                    "值星帶",
+                    "場地費尾款",
+                    "紅包",
+                  ], //顯示區間名稱
+                  datasets: [
+                    {
+                      label: "108學年度", // tootip 出現的名稱
+                      lineTension: 0, // 曲線的彎度，設0 表示直線
+                      backgroundColor: "#07889b",
+                      borderColor: "#07889b",
+                      borderWidth: 1,
+                      data: [4100, 2300, 7138, 1240, 60430, 3300], // 資料
+                      fill: false, // 是否填滿色彩
+                    },
+                    {
+                      label: "109學年度", // tootip 出現的名稱
+                      lineTension: 0, // 曲線的彎度，設0 表示直線
+                      backgroundColor: "#e37222",
+                      borderColor: "#e37222",
+                      borderWidth: 1,
+                      data: [3200, 1400, 6238, 240, 57430], // 資料
+                      fill: false, // 是否填滿色彩
+                    },
+                  ],
+                }}
                 options={{
                   plugins: {
                     legend: {
--- src/compnents/Notifycard.js
+++ src/compnents/Notifycard.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
 import './Notifycard.css'
 import NotifyItem from './NotifyItem'
+import io from 'socket.io-client'
+const socket = io('http://localhost:4000');
+
+socket.on('組織負責人', (socket_content) => {
+    console.log(socket_content)
+})
+socket.on('1110634039', (socket_content) => {
+    console.log(socket_content)
+})
 class Notifycard extends Component {
+
     render() {
+        
         return (
             <>
                 <div className="overflow-auto ncard ">
